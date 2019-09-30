561c898d7772650bfa2509873cad3c86235c0f6c
J. De Molina Benítez <j.de.molina.benitez@avanade.com>
2019-10-01 00:22:38 +0200

    Se añaden modulo, rutas y se finaliza la navegación

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,36 +4,23 @@ import { NgModule } from '@angular/core';
 // Rutas
 import { APP_ROUTES } from './app.routes';
 
-import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Graficas1Component } from './pages/graficas1/graficas1.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
-import { PagesComponent } from './pages.component';
-import { RegisterComponent } from './login/register.component';
+// Modulos
+import { PagesModule } from './pages/pages.module';
 
+import { AppComponent } from './app.component';
+import { ErrorsModule } from './errors/errors.module';
+import { LoginModule } from './login/login.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    PageNotFoundComponent,
-    DashboardComponent,
-    ProgressComponent,
-    Graficas1Component,
-    HeaderComponent,
-    SidebarComponent,
-    BreadcrumbsComponent,
-    PagesComponent,
-    RegisterComponent
   ],
   imports: [
     BrowserModule,
-    APP_ROUTES
+    APP_ROUTES,
+    PagesModule,
+    LoginModule,
+    ErrorsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
